fe332d305bd86c9602699c888d83b97f889d3831
butolen <m.butolen@htlkrems.at>
2025-05-15 11:58:19 +0200

    projektmappe und struktur erstellt

---
+++ ECommerceShop/View/Program.cs
@@ -0,0 +1,37 @@
+using ECommerceShop.Configurations;
+using Microsoft.EntityFrameworkCore;
+using View.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+builder.Services.AddDbContextFactory<ShopContext>(
+    options => options.UseMySql(
+        builder.Configuration
+            .GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8,0,27))
+    )
+);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
