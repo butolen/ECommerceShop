ba84d490c576b1d549bc24b32b69a71e991504e8
butolen <m.butolen@htlkrems.at>
2025-05-16 13:32:21 +0200

    entities

---
+++ ECommerceShop/Model/Entities/Administrator.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECommerceShop.Entities;
+[Table("administrators")]
+public class Administrator
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    
+}
