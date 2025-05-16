9d995a93412532c3551a2dd08ac2bcff809fa000
NiklasGoll <niklasperteneder@gmail.com>
2025-05-16 13:58:52 +0200

    reviews done

---
+++ ECommerceShop/Model/Entities/reviews.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECommerceShop.Entities;
+
+[Table("reviews")]
+public class reviews
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int reviewId { get; set; }
+    
+    [Required]
+    public int rating { get; set; }
+    
+    [Required]
+    [StringLength(50)]
+    public string user { get; set; }
+}
