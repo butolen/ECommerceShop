b5e987e03c78428021932d5e0291356eb8050372
Sandla187 <d.thielmann@htlkrems.at>
2025-05-19 15:15:39 +0200

    IStoreService.cs filled

--- ECommerceShop/Model/DLL/IStoreService.cs
+++ ECommerceShop/Model/DLL/IStoreService.cs
@@ -1,10 +1,31 @@
-﻿namespace ECommerceShop.DLL;
+﻿using ECommerceShop.Entities;
+using System.Collections.Generic;
 
-public interface IStoreService
+namespace ECommerceShop.DLL
 {
-    //login 
-    // SearchByName
-    //SearchByCategory
-    //SearchByPrice
-    
+    public interface IStoreService
+    {
+        // Authentifizierung & Rollen
+        bool Login(string email, string password, out string role);
+        string GetUserRole(string email);
+
+        // Produktsuche
+        List<Product> SearchByName(string name);
+        List<Product> SearchByCategory(string category);
+        List<Product> SearchByPrice(decimal minPrice, decimal maxPrice);
+        List<Product> SearchByRating(int minStars);
+
+        // Admin
+        void AddProduct(Product product, string adminEmail);
+        bool DeleteUser(string username);
+
+        // Warenkorb
+        void AddToCart(string username, int productId, int quantity);
+        void RemoveFromCart(string username, int productId);
+        void ChangeQuantity(string username, int productId, int newQuantity);
+        bool CompletePurchase(string username);
+
+        // Bewertung
+        void WriteReview(string username, int productId, int rating, string comment);
+    }
 }
